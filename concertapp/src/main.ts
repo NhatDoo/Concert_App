2594d8df4247b7b3ea724dc8cbb934eaa6722d03
NhatDoo <124895613+NhatDoo@users.noreply.github.com>
2026-04-13 16:58:39 +0000

    fix CORS bug

--- concertapp/src/main.ts
+++ concertapp/src/main.ts
@@ -10,8 +10,21 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new DomainExceptionFilter());
   // Enable CORS cho phép Frontend gọi API
+  const corsOrigins = [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL,
+  ].filter(Boolean);
+
+  // Support GitHub Codespaces pattern
+  if (process.env.CODESPACE_NAME) {
+    corsOrigins.push(
+      `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`,
+    );
+  }
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    // origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: corsOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
